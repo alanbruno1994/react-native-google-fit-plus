8598d6f272d967bd79f06ab8d53f43255ab59e21
alanbruno1994 <bruno47rios@outlook.com>
2023-02-10 15:14:18 -0300

    feat: add class HeartHistory

---
+++ android/src/main/java/com/reactnative/googlefit/HeartHistory.java
@@ -0,0 +1,59 @@
+package com.reactnative.googlefit;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.*;
+import com.google.android.gms.auth.api.signin.*;
+import com.google.android.gms.fitness.*;
+import com.google.android.gms.fitness.data.*;
+import com.google.android.gms.fitness.request.*;
+import com.google.android.gms.tasks.*;
+
+
+import java.util.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.google.android.gms.fitness.data.Device.TYPE_WATCH;
+
+public class HeartHistory {
+    private ReactContext mReactContext;
+    private GoogleFitManager googleFitManager;
+    private static final String TAG = "RNGoogleFit";
+    private static final String HEARTBEAT_FIELD_NAME = "heartbeat";
+
+    public HeartHistory(ReactContext reactContext, GoogleFitManager googleFitManager){
+        this.mReactContext = reactContext;
+        this.googleFitManager = googleFitManager;
+    }
+
+    public void saveHeart(long startTime,long endTime, float value, Promise promise) {
+        DataSource dataSource = new DataSource.Builder()
+                .setAppPackageName(this.mReactContext.getPackageName())
+                .setDataType(DataType.TYPE_HEART_RATE_BPM)
+                .setType(DataSource.TYPE_RAW)
+                .build();
+
+        DataSet dataSet = DataSet.create(dataSource);
+        DataPoint dataPoint = dataSet.createDataPoint().setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS);
+        dataPoint.getValue(Field.FIELD_BPM).setFloat(value);
+        dataSet.add(dataPoint);
+
+        Fitness.getHistoryClient(this.mReactContext, GoogleSignIn.getLastSignedInAccount(this.mReactContext))
+                .insertData(dataSet)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                       promise.resolve(true);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        promise.resolve(reject);
+                    }
+                });
+    }
+
+
+}
+
