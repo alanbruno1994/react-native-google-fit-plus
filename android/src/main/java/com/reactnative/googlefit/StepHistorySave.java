ef00f65dcf555f43f2d07fb93fa4b533b0a46f68
alanbruno1994 <bruno47rios@outlook.com>
2023-03-03 07:41:58 -0300

    fix: solve problem in logic

--- android/src/main/java/com/reactnative/googlefit/StepHistorySave.java
+++ android/src/main/java/com/reactnative/googlefit/StepHistorySave.java
@@ -17,47 +17,53 @@ import java.util.concurrent.TimeUnit;
 import static com.google.android.gms.fitness.data.Device.TYPE_WATCH;
 
 public class StepHistorySave {
-        private ReactContext mReactContext;
-        private GoogleFitManager googleFitManager;
-        private static final String TAG = "RNGoogleFit";
- 
+    private ReactContext mReactContext;
+    private GoogleFitManager googleFitManager;
+    private static final String TAG = "Step";
 
-        public StepHistorySave(ReactContext reactContext, GoogleFitManager googleFitManager){
-            this.mReactContext = reactContext;
-            this.googleFitManager = googleFitManager;
+
+    public StepHistorySave(ReactContext reactContext, GoogleFitManager googleFitManager) {
+        this.mReactContext = reactContext;
+        this.googleFitManager = googleFitManager;
+    }
+
+    public void saveStep(long startTime, long endTime, int step, Promise promise) {
+        try {
+            DataSource dataSource = new DataSource.Builder()
+                    .setAppPackageName(mReactContext.getPackageName())
+                    .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                    .setType(DataSource.TYPE_RAW)
+                    .build();
+
+
+            DataSet stepCountDeltaDataSet = DataSet.create(dataSource);
+
+            DataPoint stepCountDelta = stepCountDeltaDataSet.createDataPoint().setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS);
+            stepCountDelta.getValue(Field.FIELD_STEPS).setInt(step);
+            stepCountDeltaDataSet.add(stepCountDelta);
+
+
+            // Inserir o conjunto de dados na conta do usuário
+            Fitness.getHistoryClient(mReactContext, GoogleSignIn.getLastSignedInAccount(mReactContext))
+                    .insertData(stepCountDeltaDataSet)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "Dados de passos inseridos com sucesso.");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e(TAG, "Erro ao inserir dados de passos.", e);
+                        }
+                    });
+
+
+        } catch (Exception e) {
+
         }
-
-        public void saveStep(long startTime,long endTime, int step, Promise promise) {
-           DataSource dataSource = new DataSource.Builder()
-                   .setAppPackageName(mReactContext)
-                   .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
-                   .build();
-           DataPoint stepCountDelta = DataPoint.builder(dataSource)
-                   .setField(Field.FIELD_STEPS, step)
-                   .build();
-
-           // Criar um conjunto de dados com o DataPoint e o intervalo de tempo
-           DataSet stepCountDeltaDataSet = DataSet.builder(dataSource)
-                   .add(stepCountDelta)
-                   .setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS)
-                   .build();
-
-           // Inserir o conjunto de dados na conta do usuário
-           Fitness.getHistoryClient(mReactContext, GoogleSignIn.getLastSignedInAccount(mReactContext))
-                   .insertData(stepCountDeltaDataSet)
-                   .addOnSuccessListener(new OnSuccessListener<Void>() {
-                       @Override
-                       public void onSuccess(Void aVoid) {
-                           Log.d(TAG, "Dados de passos inseridos com sucesso.");
-                       }
-                   })
-                   .addOnFailureListener(new OnFailureListener() {
-                       @Override
-                       public void onFailure(Exception e) {
-                           Log.e(TAG, "Erro ao inserir dados de passos.", e);
-                       }
-                   });
-        }
+    }
 
 
 }
